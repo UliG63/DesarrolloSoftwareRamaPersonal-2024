1b7444480968b83f6c8fdbc69e1924e6e90c0113
Mauro-Dorigoni <dorigonimauro@gmail.com>
2024-05-23 12:58:11 -0300

    API Rest para mago completa

--- TPI/Backend/APIReST/dist/app.js
+++ TPI/Backend/APIReST/dist/app.js
@@ -6,6 +6,18 @@ const magos = [
     new Magos('Harry', 'Potter', ['Acebo', 'Pluma de Fenix', '28cm']),
     new Magos('Albus Percival Wulfric Brian', 'Dumbledore', ['Sauco', 'Pelo de cola de Thestral', '34cm']),
 ];
+function sanitizeMagoInput(req, res, next) {
+    req.body.sanitizedInput = {
+        name: req.body.name,
+        apellido: req.body.apellido,
+        varita: req.body.varita,
+    };
+    Object.keys(req.body.sanitizedInput).forEach(key => {
+        if (req.body.sanitizedInput[key] === undefined)
+            delete req.body.sanitizedInput[key];
+    });
+    next();
+}
 app.get('/api/magos', (req, res) => {
     res.json({ data: magos });
 });
@@ -16,11 +28,35 @@ app.get('/api/magos/:id', (req, res) => {
     }
     res.json({ data: mago });
 });
-app.post('/api/magos', (req, res) => {
-    const { name, apellido, varita } = req.body;
-    const mago = new Magos(name, apellido, varita);
+app.post('/api/magos', sanitizeMagoInput, (req, res) => {
+    const input = req.body.sanitizedInput;
+    const mago = new Magos(input.name, input.apellido, input.varita);
     magos.push(mago);
     res.status(201).send({ message: 'Mago Creado', data: mago });
+});
+app.put('/api/magos/:id', sanitizeMagoInput, (req, res) => {
+    const magoIdx = magos.findIndex((mago) => { mago.idMago === req.params.id; });
+    if (magoIdx === -1) {
+        res.status(404).send({ message: 'Mago not Found' });
+    }
+    magos[magoIdx] = { ...magos[magoIdx], ...req.body.sanitizedInput };
+    res.status(200).send({ message: "Mago actualizado correctamente", data: magos[magoIdx] });
+});
+app.patch('/api/magos/:id', sanitizeMagoInput, (req, res) => {
+    const magoIdx = magos.findIndex((mago) => { mago.idMago === req.params.id; });
+    if (magoIdx === -1) {
+        res.status(404).send({ message: 'Mago not Found' });
+    }
+    magos[magoIdx] = { ...magos[magoIdx], ...req.body.sanitizedInput };
+    res.status(200).send({ message: "Mago actualizado correctamente", data: magos[magoIdx] });
+});
+app.delete('/api/magos/:id', (req, res) => {
+    const magoIdx = magos.findIndex((mago) => { mago.idMago === req.params.id; });
+    if (magoIdx === -1) {
+        res.status(404).send({ message: 'Mago not Found' });
+    }
+    magos.splice(magoIdx, 1);
+    res.status(200).send({ message: 'Mago eliminado exitosamente' });
 });
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000/');
--- TPI/Backend/APIReST/src/app.ts
+++ TPI/Backend/APIReST/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { Magos } from './magos.js'
 
 const app = express()
@@ -18,6 +18,21 @@ const magos: Magos[] = [
     ),
 ]
 
+function sanitizeMagoInput(req: Request, res: Response, next: NextFunction){
+    
+    req.body.sanitizedInput = {
+        name: req.body.name,
+        apellido: req.body.apellido,
+        varita: req.body.varita,
+    }
+
+    Object.keys(req.body.sanitizedInput).forEach(key=>{
+        if(req.body.sanitizedInput[key]===undefined) delete req.body.sanitizedInput[key]
+    })
+
+    next()
+}
+
 app.get('/api/magos', (req,res)=>{
     res.json({data:magos})
 })
@@ -30,14 +45,42 @@ app.get('/api/magos/:id',(req,res)=>{
     res.json({data:mago})
 })
 
-app.post('/api/magos',(req,res)=>{
-    const {name,apellido,varita}=req.body
+app.post('/api/magos',sanitizeMagoInput,(req,res)=>{
+    const input=req.body.sanitizedInput
 
     const mago=new Magos(
-        name,apellido,varita
+        input.name,input.apellido,input.varita
     )
     magos.push(mago)
     res.status(201).send({message:'Mago Creado',data:mago})
+})
+
+app.put('/api/magos/:id',sanitizeMagoInput,(req,res)=>{
+    const magoIdx = magos.findIndex((mago) => {mago.idMago === req.params.id})
+    if(magoIdx===-1){
+        res.status(404).send({message:'Mago not Found'}) 
+    }
+    magos[magoIdx] = { ...magos[magoIdx], ...req.body.sanitizedInput}
+    res.status(200).send({message:"Mago actualizado correctamente",data: magos[magoIdx] })
+})
+
+app.patch('/api/magos/:id',sanitizeMagoInput,(req,res)=>{
+    const magoIdx = magos.findIndex((mago) => {mago.idMago === req.params.id})
+    if(magoIdx===-1){
+        res.status(404).send({message:'Mago not Found'}) 
+    }
+    magos[magoIdx] = { ...magos[magoIdx], ...req.body.sanitizedInput}
+    res.status(200).send({message:"Mago actualizado correctamente",data: magos[magoIdx] })
+})
+
+app.delete('/api/magos/:id', (req,res)=>{
+    const magoIdx = magos.findIndex((mago) => {mago.idMago === req.params.id})
+    if(magoIdx===-1){
+        res.status(404).send({message:'Mago not Found'}) 
+    }
+    magos.splice(magoIdx,1)
+    res.status(200).send({message:'Mago eliminado exitosamente'})
+
 })
 
 app.listen(3000, ()=>{
