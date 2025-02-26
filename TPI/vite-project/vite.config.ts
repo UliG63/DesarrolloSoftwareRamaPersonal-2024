66507bdc356fabb9eadbcf8939573ec19ce907aa
uli <ugimenez@frro.utn.edu.ar>
2025-02-26 08:04:13 -0300

    cambios menores para realizar el deploy tras haber implementado modulos para testing 4

--- TPI/vite-project/vite.config.ts
+++ TPI/vite-project/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig as defineVitestConfig } from 'vitest/config';
+//import { defineConfig as defineVitestConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
