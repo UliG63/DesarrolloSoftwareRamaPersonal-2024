8e80f864caaa9a66342a11d8a0ccadf03729b9f5
uli <ugimenez@frro.utn.edu.ar>
2025-02-27 14:28:35 -0300

    Incremento en el tiempo de animacion del showSPinner a 3000 mseg

--- TPI/vite-project/src/components/loadingSpinner/loadingSpinner.tsx
+++ TPI/vite-project/src/components/loadingSpinner/loadingSpinner.tsx
@@ -6,7 +6,7 @@ const LoadingSpinner = () => {
             <RotatingLines
                 strokeColor="#7f4e2b"
                 strokeWidth="5"
-                animationDuration="500.0"
+                animationDuration="0.75"
                 width="96"
                 visible={true}
             />
