9d3a3ad035a834ea1641c173db7d6711d58a23ff
uli <ugimenez@frro.utn.edu.ar>
2025-02-27 14:13:30 -0300

    Incremento en el tiempo de animacion del spinner a 1.5

--- TPI/vite-project/src/components/loadingSpinner/loadingSpinner.tsx
+++ TPI/vite-project/src/components/loadingSpinner/loadingSpinner.tsx
@@ -6,7 +6,7 @@ const LoadingSpinner = () => {
             <RotatingLines
                 strokeColor="#7f4e2b"
                 strokeWidth="5"
-                animationDuration="0.90"
+                animationDuration="1.5"
                 width="96"
                 visible={true}
             />
