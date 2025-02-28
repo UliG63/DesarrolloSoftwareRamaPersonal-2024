fa1682962a4fb9404e9fab351c4476a4292c0775
uli <ugimenez@frro.utn.edu.ar>
2025-02-28 18:23:13 -0300

    Agregado de lazyLoading para la carga del app.tsx

--- TPI/vite-project/src/App.tsx
+++ TPI/vite-project/src/App.tsx
@@ -1,22 +1,24 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import LoginPage from './pages/login';
-import HechizosPage from './pages/hechizos';
-import HechizosEmpleadoPage from './pages/hechizosEmpleado';
-import PatentesPage from './pages/patentes';
-import PatentesEmpleadoPage from './pages/patentesEmpleado';
-import VisualizacionPage from './pages/visualizacion';
-import VisualizacionEmpleadoPage from './pages/visualizacionEmpleado';
-import InicioPage from './pages/inicio';
-import UserPage from './pages/user';
-import MagosPage from './pages/magos';
-import InstitucionesPage from './pages/instituciones';
-import EtiquetasPage from './pages/etiquetas';
-import TipoHechizoPage from './pages/tipoHechizo';
 import { AuthContextProvider, AuthContext } from './context/authContext';
-import MagosEmpleado from './pages/magosEmpleado';
 
-// Verifica la autenticación, el rol y selecciona la página según el rol
+// Carga diferida de las páginas
+const LoginPage = lazy(() => import('./pages/login'));
+const HechizosPage = lazy(() => import('./pages/hechizos'));
+const HechizosEmpleadoPage = lazy(() => import('./pages/hechizosEmpleado'));
+const PatentesPage = lazy(() => import('./pages/patentes'));
+const PatentesEmpleadoPage = lazy(() => import('./pages/patentesEmpleado'));
+const VisualizacionPage = lazy(() => import('./pages/visualizacion'));
+const VisualizacionEmpleadoPage = lazy(() => import('./pages/visualizacionEmpleado'));
+const InicioPage = lazy(() => import('./pages/inicio'));
+const UserPage = lazy(() => import('./pages/user'));
+const MagosPage = lazy(() => import('./pages/magos'));
+const InstitucionesPage = lazy(() => import('./pages/instituciones'));
+const EtiquetasPage = lazy(() => import('./pages/etiquetas'));
+const TipoHechizoPage = lazy(() => import('./pages/tipoHechizo'));
+const MagosEmpleado = lazy(() => import('./pages/magosEmpleado'));
+
+// Verifica la autenticación y selecciona la página según el rol
 const ProtectedRoute: React.FC<{ children: React.ReactNode; empleadoPage?: React.ReactNode; isEmpleadoOnly?: boolean }> = ({ children, empleadoPage, isEmpleadoOnly }) => {
   const { currentUser } = useContext(AuthContext);
 
@@ -40,7 +42,7 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode; empleadoPage?: React
 };
 
 const App: React.FC = () => {
-  //Logica para asegurarme de que se carguen los estilos antes del renderizado
+  // Lógica para asegurarme de que los estilos se carguen antes del renderizado
   const [isStylesLoaded, setIsStylesLoaded] = useState(false);
 
   useEffect(() => {
@@ -56,13 +58,13 @@ const App: React.FC = () => {
   }, []);
 
   if (!isStylesLoaded) {
-    return <div className="loading-screen">Cargando...</div>; // Muestra un loader mientras los estilos se cargan
+    return <div className="loading-screen">Cargando...</div>; // Loader mientras los estilos se cargan
   }
 
   return (
     <AuthContextProvider>
       <Router>
-        <div>
+        <Suspense fallback={<div className="loading-screen">Cargando página...</div>}>
           <Routes>
             <Route path="/" element={<ProtectedRoute><InicioPage /></ProtectedRoute>} />
             <Route path="/login" element={<LoginPage />} />
@@ -76,7 +78,7 @@ const App: React.FC = () => {
             <Route path="/tipo_hechizo" element={<ProtectedRoute isEmpleadoOnly={true}><TipoHechizoPage /></ProtectedRoute>} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
-        </div>
+        </Suspense>
       </Router>
     </AuthContextProvider>
   );
